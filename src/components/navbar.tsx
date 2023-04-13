aa9e6abf4ffc716df655bd1f1986a5eb62e5f1f2
nyovhz <vhz.cfeteit@gmail.com>
2023-04-13 00:19:33 -0600

    update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
 import { GiBrain } from "react-icons/gi";
 
-export default function NavBar(){
+function Button( {name, addr} : any ) {
+    return (
+        <li className="header_section_name">
+            <Link href={addr}>{name}</Link>
+        </li>
+    )
+}
+
+function NavBar(){
     return(
         <nav className="header">
             <div>
                 <GiBrain className="header_logo"/>
             </div>
             <ul className="md:flex md:items-center gap-10">
-                <Button addr="/####" name="Rick&Morty"/>
+                <Button addr="/####" name="Rick & Morty"/>
                 <Button addr="/" name="Calculadora"/>
                 <Button addr="/" name="Numeros Primos"/>
             </ul>
@@ -16,10 +24,4 @@ export default function NavBar(){
     )
 }
 
-function Button( {name, addr}:any ) {
-    return (
-        <li className="header_section_name">
-            <Link href={addr}>{name}</Link>
-        </li>
-    )
-}
+export default NavBar
